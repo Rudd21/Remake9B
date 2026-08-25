caef3b26aca74ba45c54745973805c750d48ab58
rudd21 <yackovschool@gmail.com>
2026-08-25 11:18:52 +0300

    Test React rendering

--- src/main.jsx
+++ src/main.jsx
@@ -12,19 +12,22 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 console.log('ROOT:', document.getElementById('root'))
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
+  <h1>TEST</h1>
+  // <React.StrictMode>
+  //   <BrowserRouter>
+  //     <Routes>
+  //       <Route path="/" element={<App />}>
 
-          <Route index element={<Home />} />
-          <Route path="/about_site" element={<AboutSite />} />
-          <Route path="/classmates" element={<Classmates />} />
-          <Route path="/oldGuard" element={<OldGuard />} />
-          <Route path="/gallery" element={<Gallery />} />
+  //         <Route index element={<Home />} />
+  //         <Route path="/about_site" element={<AboutSite />} />
+  //         <Route path="/classmates" element={<Classmates />} />
+  //         <Route path="/oldGuard" element={<OldGuard />} />
+  //         <Route path="/gallery" element={<Gallery />} />
         
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  //       </Route>
+  //     </Routes>
+  //   </BrowserRouter>
+  // </React.StrictMode>
 );
+
+console.log('AFTER ROOT')
